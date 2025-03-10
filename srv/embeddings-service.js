1d0958175e3e0e7442ace34b7d491a69d140a60d
Gabriel Freire <gabriel.freire@lab2dev.com>
2025-03-10 02:22:14 +0000

    chore: add documentation

--- srv/embeddings-service.js
+++ srv/embeddings-service.js
@@ -1,5 +1,5 @@
-const llmGateway = require("./gateways/LLMGateway");
-const vectorChunkRepository = require("./repositories/DocumentChunkRepository");
+const gateway = require("./gateways/LLMGateway");
+const repository = require("./repositories/DocumentChunkRepository");
 const bufferAdapter = require("./adapters/BufferAdapter");
 const splitterAdapter = require("./adapters/SplitterAdapter")
 
@@ -12,7 +12,7 @@ module.exports = function () {
       const textChunks = await splitterAdapter.splitText(content);
 
       const textChunkEntriesPromise = textChunks.map(async chunk => {
-        const embeddingResult = await llmGateway.getEmbeddings({ chunk });
+        const embeddingResult = await gateway.getEmbeddings({ chunk });
         const embedding = embeddingResult?.data[0]?.embedding;
         if (!embedding) throw new Error(`Embedding not found for chunk: ${chunk}`);
 
@@ -23,7 +23,7 @@ module.exports = function () {
       });
 
       const textChunkEntries = await Promise.all(textChunkEntriesPromise);
-      await vectorChunkRepository.create(DocumentChunk, textChunkEntries);
+      await repository.create(DocumentChunk, textChunkEntries);
       _.res.status(201).json();
     } catch (error) {
       console.error("Error in /embeddings/generate:", error);
